ff57739e5b92390af80ffb128bf5604f8b6caad0
Marbo Cheng Ye <marbochengye@gmail.com>
2019-07-29 15:10:33 -0700

    MVP two sum

--- Easy-Most-Frequent/1.TwoSum.js
+++ Easy-Most-Frequent/1.TwoSum.js
@@ -10,3 +10,23 @@ Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    
+    for (var i = 0; i < nums.length; i++) {
+        
+        for (var x = i + 1; x < nums.length; x++) {
+            var num1= nums[i];
+            var num2 = nums[x];
+            
+            if (num1 + num2 === target) {
+                return [i,x];
+            }
+            
+        }
+    }
+};
