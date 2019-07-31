f280ba3424b24449dc9a2605e027dd458242caf0
Marbo Cheng Ye <marbochengye@gmail.com>
2019-07-30 21:56:23 -0700

    Attempt move zero problem

--- Easy-Most-Frequent/283.MoveZeroes.js
+++ Easy-Most-Frequent/283.MoveZeroes.js
@@ -11,3 +11,26 @@ Note:
 You must do this in-place without making a copy of the array.
 Minimize the total number of operations.
 */
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    for (var i = 0; i < nums.length; i++) {
+        var current = nums[i];
+        
+        if (nums[i] === 0) {
+            var l = i;
+            //swap current with next until reach last position of array
+            for (var x = i+1; x < nums.length; x++) {
+                
+                nums[l] = nums[x];
+                
+                l++;
+            }
+            console.log(nums);
+            nums[nums.length-1] = 0;
+        }
+    }
+    
+};
