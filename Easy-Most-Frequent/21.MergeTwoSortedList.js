86d07001d2dca9ee11a1c0d4465a73892629c5eb
Marbo Cheng Ye <marbochengye@gmail.com>
2019-07-30 21:35:57 -0700

    Valid parenthesis problem

--- Easy-Most-Frequent/21.MergeTwoSortedList.js
+++ Easy-Most-Frequent/21.MergeTwoSortedList.js
@@ -7,3 +7,19 @@ Example:
 Input: 1->2->4, 1->3->4
 Output: 1->1->2->3->4->4
 */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+    
+};
