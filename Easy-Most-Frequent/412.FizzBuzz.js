fcdad97653f27098ed68631f456913bc2f17b829
Marbo Cheng Ye <marbochengye@gmail.com>
2019-07-30 22:03:46 -0700

    Fizz Buzz mvp

--- Easy-Most-Frequent/412.FizzBuzz.js
+++ Easy-Most-Frequent/412.FizzBuzz.js
@@ -27,3 +27,25 @@ Return:
     "FizzBuzz"
 ]
 */
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+    var result = [];
+    
+    for (var i = 1; i <= n; i++) {
+        if (i % 15 === 0) {
+            result.push('FizzBuzz');
+        } else if (i % 5 === 0)  {
+           
+            result.push('Buzz');
+            
+        } else if ( i % 3 === 0){
+            result.push('Fizz');
+        } else {
+            result.push(i+'');
+        }
+    }
+    return result;
+};
