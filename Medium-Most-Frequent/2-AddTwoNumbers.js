eca624f0d50b39b313a3a88489ae0bc24d66fe74
Marbo Cheng Ye <marbochengye@gmail.com>
2019-07-30 21:31:29 -0700

    Attempt add two number problem

--- Medium-Most-Frequent/2-AddTwoNumbers.js
+++ Medium-Most-Frequent/2-AddTwoNumbers.js
@@ -10,3 +10,77 @@ Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
 Output: 7 -> 0 -> 8
 Explanation: 342 + 465 = 807.
 */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    var n1 = l1;
+    var n2 = l2;
+    
+    let number1 = '';
+    let number2 = '';
+    
+    while (n1 !== null) {
+        number1 += n1.val;
+       
+        
+        n1 = n1.next;
+        
+    }
+    while (n2!==null){
+        number2 += n2.val;
+        n2 = n2.next;
+    }
+    
+    console.log( number1);
+    console.log( number2);
+    let number = Number(number1) + Number(number2);
+    number = number.toString().split('');
+    //console.log(number);
+    //console.log(number[number.length-1]);
+    let listHead ;
+    let list ;
+    
+    if(number2 !== '0' && number1 !== '0') {
+        for (var i = number.length-1; i >=0 ; i--) {
+        if (i === number.length-1){
+             listHead= new ListNode(number[number.length-1]);
+            list = listHead;
+        }
+          
+        else {
+            list.next = new ListNode(number[i]);
+            list = list.next;
+        }
+            
+    }
+    }
+   
+     else {
+        for (var i = 0; i < number.length ; i++) {
+        if (i === 0){
+             listHead= new ListNode(number[i]);
+            list = listHead;
+        }
+          
+        else {
+            list.next = new ListNode(number[i]);
+            list = list.next;
+        }
+        
+    }
+        
+    }
+
+    
+    return listHead;
+};
