b343350132b3c877ab9916013989128c5a5ddfc2
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-04 13:54:52 -0700

    MVP kth largest in array

--- Medium-Most-Frequent/215-Kth-Largest-Element-in-an-Array.js
+++ Medium-Most-Frequent/215-Kth-Largest-Element-in-an-Array.js
@@ -13,3 +13,26 @@ Output: 4
 Note: 
 You may assume k is always valid, 1 ≤ k ≤ array's length.
 */
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function(nums, k) {
+    
+    for (var x = 0; x < k; x++) {
+           for (var i = 0; i < nums.length-1; i++) {
+        let current = nums[i];
+        let next =nums[i+1];
+        
+        if (current > next) {
+            let temp = next;
+            nums[i+1] = current;
+            nums[i] = temp;
+        }
+        }
+    }
+    return nums[nums.length-k];
+ 
+    
+};
