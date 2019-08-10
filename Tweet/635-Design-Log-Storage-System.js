f8ca9a064c58bfde32b7e920725ec280d943a986
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-10 00:00:27 -0700

    MVP log storage problem

--- Tweet/635-Design-Log-Storage-System.js
+++ Tweet/635-Design-Log-Storage-System.js
@@ -1,35 +1,3 @@
-/*
-You are given several logs that each log contains a unique id and timestamp. Timestamp is 
-a string that has the following format: Year:Month:Day:Hour:Minute:Second, for example, 
-2017:01:01:23:59:59. All domains are zero-padded decimal numbers.
-
-Design a log storage system to implement the following functions:
-
-void Put(int id, string timestamp): Given a log's unique id and timestamp, store the log in 
-your storage system.
-
-
-int[] Retrieve(String start, String end, String granularity): Return the id of logs whose 
-timestamps are within the range from start to end. Start and end all have the same format 
-as timestamp. However, granularity means the time level for consideration. For example, 
-start = "2017:01:01:23:59:59", end = "2017:01:02:23:59:59", granularity = "Day", it means 
-that we need to find the logs within the range from Jan. 1st 2017 to Jan. 2nd 2017.
-
-Example 1:
-put(1, "2017:01:01:23:59:59");
-put(2, "2017:01:01:22:59:59");
-put(3, "2016:01:01:00:00:00");
-retrieve("2016:01:01:01:01:01","2017:01:01:23:00:00","Year"); // return [1,2,3], because you 
-need to return all logs within 2016 and 2017.
-retrieve("2016:01:01:01:01:01","2017:01:01:23:00:00","Hour"); // return [1,2], because you 
-need to return all logs start from 2016:01:01:01 to 2017:01:01:23, where log 3 is left 
-outside the range.
-
-Note:
-There will be at most 300 operations of Put or Retrieve.
-Year ranges from [2000,2017]. Hour ranges from [00,23].
-Output for Retrieve has no order required.
-*/
 
 var LogSystem = function() {
     this.log = {};
@@ -74,16 +42,42 @@ LogSystem.prototype.retrieve = function(s, e, gra) {
         var time = logTime.split(':');
       
         var inRange = true;
-        for (var i = 0; i < granular ; i++) {
-            if (!(time[i] >= start[i] && time[i] <= end[i] )) {
-                inRange = false;
-                break;
-                }
+      
+        var totalTime = 0;
+        var startTime = 0;
+        var endTime = 0;
+        
+        var seconds = {
+            0: 365*24*60*60,
+            1: 30*24*60*60,
+            2: 24*60*60,
+            3: 60*60,
+            4: 60,
+            5: 1
         }
-        if (inRange) {
+        for (var i = 0; i < granular ; i++) {
+                 
+            totalTime += parseInt(time[i]*seconds[i]);
+            startTime += parseInt(start[i]*seconds[i]);
+            endTime += parseInt(end[i]*seconds[i]);
+            
+            // if (!(parseInt(time[i]) >= parseInt(start[i]) && parseInt(time[i]) <= parseInt(end[i]) )) {
+            //     inRange = false;
+            //     break;
+            //     }
+           
+        }
+        if (totalTime >= startTime && totalTime <= endTime) {
             result.push(key);
         }
         
     }
     return result;
 };
+
+/** 
+ * Your LogSystem object will be instantiated and called as such:
+ * var obj = new LogSystem()
+ * obj.put(id,timestamp)
+ * var param_2 = obj.retrieve(s,e,gra)
+ */
