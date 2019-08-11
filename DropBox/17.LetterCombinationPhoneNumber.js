f8c81d907c8ea1c744b315fff27c258bbdc21a21
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-11 00:20:15 -0700

    Phone number and their associated letters

--- DropBox/17.LetterCombinationPhoneNumber.js
+++ DropBox/17.LetterCombinationPhoneNumber.js
@@ -5,6 +5,14 @@ the number could represent.
 A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does 
 not map to any letters.
 
+2: abc
+3: def
+4: ghi
+5: jkl
+6: mno
+7: pqrs
+8: tuv
+9: wxyz
 
 
 Example:
