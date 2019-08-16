cbd34bd323caa9db97ef9f436079017ee6f8a4ef
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-16 00:24:32 -0700

    MVP max subarray problem

--- SAP/Easy/53.MaxSubArray.js
+++ SAP/Easy/53.MaxSubArray.js
@@ -12,3 +12,28 @@ Follow up:
 If you have figured out the O(n) solution, try coding another solution using the divide and 
 conquer approach, which is more subtle.
 */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    
+    
+    if (root === null) {
+        return 0;
+    } else {
+        let left = maxDepth(root.left);
+        let right = maxDepth(root.right);
+        
+        return Math.max(left,right) + 1;
+    }
+ 
+    
+};
