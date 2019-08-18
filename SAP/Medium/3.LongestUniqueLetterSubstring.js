53ec1a32ff5d3b625b285ac99d6946b1bf774ddc
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-17 21:38:20 -0700

    MVP longestuniqure substring problem

--- SAP/Medium/3.LongestUniqueLetterSubstring.js
+++ SAP/Medium/3.LongestUniqueLetterSubstring.js
@@ -18,3 +18,36 @@ Output: 3
 Explanation: The answer is "wke", with the length of 3. 
              Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 */
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let letters = {};
+    let maxSubstringLength = 0;
+    
+    
+    for (let i = 0; i < s.length; i++) {
+        let letters = {};
+        let currentLength = 1;
+        letters[s[i]] = 1;
+        
+        for (let y = i+1; y < s.length; y++) {
+            if (letters[s[y]] === undefined) {
+                letters[s[y]] = 1;
+                currentLength++;
+                
+              
+            } else {
+                letters = {};
+                break;
+            }
+            
+        }
+         if (currentLength > maxSubstringLength) {
+                maxSubstringLength = currentLength;
+        }
+    }
+    return maxSubstringLength;
+   
+};
