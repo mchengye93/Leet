44d5d01cd921af0de9e48cb0509174206402c87a
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-21 22:37:24 -0700

    remove Elements function setup

--- LinkedList/203.RemoveLinkedListElement.js
+++ LinkedList/203.RemoveLinkedListElement.js
@@ -6,3 +6,18 @@ Example:
 Input:  1->2->6->3->4->5->6, val = 6
 Output: 1->2->3->4->5
 */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+    
+};
