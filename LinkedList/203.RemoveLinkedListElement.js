62540213eed1157f00fbe57a8a7eac1212c765e5
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-21 22:46:36 -0700

    Remove element attempt

--- LinkedList/203.RemoveLinkedListElement.js
+++ LinkedList/203.RemoveLinkedListElement.js
@@ -14,10 +14,43 @@ Output: 1->2->3->4->5
  * }
  */
 /**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
  * @param {ListNode} head
  * @param {number} val
  * @return {ListNode}
  */
 var removeElements = function(head, val) {
     
+    if (head !==null){
+    var current = head;
+    var next = current.next;
+        
+    if (current.val === val) {
+        current = next;
+        if(next !== null){
+         next = next.next;
+        }
+       
+    }
+    
+    while (next !== null) {
+        if (next.val === val) {
+            current.next = next.next;
+            next = next.next;
+        } else {
+            current = next;
+            next = next.next;
+        }
+    }
+    }
+   
+    
+  
+    return head;
 };
